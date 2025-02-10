9828af6adcc26ef85945e9ad01da1774ae529726
r-david8 <remesdavid8@gmail.com>
2025-02-10 17:01:20 +0100

    proba dolgozat

--- dolgozat/src/app/child/child.component.ts
+++ dolgozat/src/app/child/child.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-child',
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './child.component.css'
 })
 export class ChildComponent {
-
+  @Input() childText: string = '';
 }
--- dolgozat/src/app/parent/parent.component.ts
+++ dolgozat/src/app/parent/parent.component.ts
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './parent.component.css'
 })
 export class ParentComponent {
-
+  inputText: string = '';
 }
